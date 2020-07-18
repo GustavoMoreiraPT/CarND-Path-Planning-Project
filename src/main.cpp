03b725cc0b69374069b8242b07091f7c67ba0513
Gustavo <gustavo1995.2012@hotmail.com>
2020-07-18 17:51:40 +0100

    Solving cold start. Velocity is now realistic

--- src/main.cpp
+++ src/main.cpp
@@ -54,7 +54,7 @@ int main() {
   //start in lane 1 for now
   int lane = 1;
 
-  double ref_vel = 49.5;
+  double ref_vel = 0.0;
 
   h.onMessage([&map_waypoints_x,&map_waypoints_y,&map_waypoints_s,
                &map_waypoints_dx,&map_waypoints_dy]
@@ -96,6 +96,37 @@ int main() {
 
           //previous path size of the car (last known points that he knew)
           int prev_size = previous_path_x.size();
+
+          if(prev_size > 0){
+            car_s = end_path_s  
+		  }
+
+          bool too_close = false;
+
+          //find ref_v to use
+          for(int i = 0; i < sensor_fusion.size(); i++){
+            //car is in my lane
+            float d = sensor_fusion[i][6];
+            if(d < (2+4*lane+2) && d > (2+4*lane-2)){
+                double vx = sensor_fusion[i][3];
+                double vy = sensor_fusion[i][4];
+                double check_speed = sqrt(vx*vx+vy*vy);
+                double check_car_s = sensor_fusion[i][5];
+
+                check_car_s+=((double)prev_size*.02*check_speed);
+                //check s values f=greater than mine and s gap
+                if((check_car_s > car_s) && (check_car_s-car_s) < 30){
+                    //ref_vel = 29.5;     
+                    too_close = true;
+				}
+			}
+		  }
+
+          if(too_close){
+            ref_vel -= .224;  
+		  }else if(ref_vel < 49.5){
+              ref_vel += .224;
+		  }
 
           json msgJson;
 
